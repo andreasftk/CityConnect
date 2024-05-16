4656442ad7c1e8695d06a4176d9e4dde56575354
andreasftk <68335410+andreasftk@users.noreply.github.com>
2024-05-16 22:36:17 +0300

    MainPage small changes

--- code/app/src/main/java/cityconnnect/app/MainPage.kt
+++ code/app/src/main/java/cityconnnect/app/MainPage.kt
@@ -1,15 +1,36 @@
 package cityconnnect.app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import cityconnnect.app.databinding.ActivityMainPageBinding
+import cityconnnect.app.ui.MainBus
 
 class MainPage : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        binding = ActivityMainPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bus = findViewById<Button>(R.id.bus_btn)
+
+        bus.setOnClickListener {
+            val intent = Intent(this, MainBus::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
+
         setContentView(R.layout.activity_main_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
---
+++ code/app/src/main/java/cityconnnect/app/ui/MainBus.kt
@@ -0,0 +1,4 @@
+package cityconnnect.app.ui
+
+class MainBus {
+}
